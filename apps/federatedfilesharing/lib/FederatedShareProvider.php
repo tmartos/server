a33da2e2ca2deb13241086f32aabd9b512e8b5fe
Robin Appelman <icewind@owncloud.com>
2016-06-18 22:04:56 +0200

    Add getShareTypesInFolder to optimize folder listening

--- apps/federatedfilesharing/lib/FederatedShareProvider.php
+++ apps/federatedfilesharing/lib/FederatedShareProvider.php
@@ -560,6 +560,11 @@ class FederatedShareProvider implements IShareProvider {
 		return;
 	}
 
+
+	public function getSharesInFolder($userId, $node, $reshares) {
+		return [];//TODO
+	}
+
 	/**
 	 * @inheritdoc
 	 */
